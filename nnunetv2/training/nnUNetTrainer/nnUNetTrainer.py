50f1d3696873c35a0b0f055f6c503903a7dfd873
behnaz-motavali <bs.motavali@gmail.com>
2025-06-01 12:25:58 +0200

    fixed my_init_kwargs

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -116,8 +116,7 @@ class nnUNetTrainer(object):
         # need. So let's save the init args
         self.my_init_kwargs = {}
         for k in inspect.signature(self.__init__).parameters.keys():
-            if k in locals():
-                self.my_init_kwargs[k] = locals()[k]
+            self.my_init_kwargs[k] = locals()[k]    
 
         ###  Saving all the init args into class variables for later access
         self.plans_manager = PlansManager(plans)
